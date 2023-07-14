1c758546eb7ccce04453bb4de3a161592a34d286
ximena saldarriaga <xime-2208@hotmail.com>
2023-07-14 16:56:09 -0500

    add creations

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Header from '../header/Header.jsx'
 import Info from '../info/Info.jsx'
 import Footer from '../footer/Footer.jsx'
+import Creations from '../creations/Creations.jsx'
 import './home.scss'
 
 const Home = () => {
   return (
     <>
-    <div className='home'>
+    <div>
     <Header />
     <Info />
+    <Creations />
     <Footer />
     </div>
     </>
